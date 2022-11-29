7ac072d2d82831598c9edf0deb4b82b6126afcc8
argelignacio <iargel@fi.uba.ar>
2022-11-28 21:05:05 -0300

    adding block user

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { DataGrid } from "@mui/x-data-grid";
-import { get } from "../utils/requests";
+import { get, patch } from "../utils/requests";
 import NavBar from "./NavBar";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { setState } from "../utils/setState";
 import { config } from "../Constants";
 import CustomizedMenus from "./microcomponents/desplegable.jsx"
-
-import DeleteIcon from '@mui/icons-material/Delete';
 import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
 import InfoIcon from '@mui/icons-material/Info';
+
+
 
 export default function DataTable() {
   const [users, setUsers] = useState([]);
@@ -18,10 +18,16 @@ export default function DataTable() {
   const currentUserData = useSelector((store) => store.user);
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const [refresh, setRefresh] = useState(0);
 
   const goToDetails = id => () => {
 		navigate(`/detail-user/${id}`, { state: { id } });
 	};
+  
+  const block = (id, isBlocked, email) => () => {
+    const token = sessionStorage.getItem('token');
+    return patch(`${config.API_URL}/users/${id}`, token, { isBlocked: !isBlocked, email }).then(setRefresh(!refresh))
+  };
 
   const columns = [
     { field: "id", headerName: "ID", width: 100 },
@@ -29,6 +35,7 @@ export default function DataTable() {
     { field: "lastname", headerName: "Last name", width: 300 },
     { field: "email", headerName: "Email", width: 530 },
     { field: "phoneNumber", headerName: "Phone Number", width: 130 },
+    { field: "status", headerName: "Estado", width: 130 },
     {
       field: "action",
       headerName: "Acciones",
@@ -37,10 +44,8 @@ export default function DataTable() {
       renderCell: (params) => { 
         const options = [
           { name: 'Vista detallada', renderIcon: () => <InfoIcon />, callback: goToDetails(params.id) },
-          { name: 'Bloquear', renderIcon: () => <RemoveCircleOutlineIcon /> },
-          { name: 'Borrar', renderIcon: () => <DeleteIcon /> },
+          { name: 'Block/Unblock', renderIcon: () => <RemoveCircleOutlineIcon />, callback: block(params.id, params.row.isBlocked, params.row.email) }
         ];
-        
         return <CustomizedMenus options={options} />;
       },
     },
@@ -51,8 +56,23 @@ export default function DataTable() {
       const result = await get(
         `${config.API_URL}/users`,
         token
-      );
-      setUsers(result.data.data);
+      ).then(({ data: { data } }) => {
+        return data.map((user) => {
+          if (user.isBlocked){
+            return {
+              ...user,
+              status: "Inactivo"
+            }
+          }else{
+            return {
+              ...user,
+              status: "Activo"
+            }
+          }
+        })
+
+      });
+      setUsers(result);
     };
     if (sessionStorage.getItem("token")) {
       const token = sessionStorage.getItem("token");
@@ -61,7 +81,7 @@ export default function DataTable() {
     } else {
       setState(navigate, dispatch);
     }
-  }, []);
+  }, [ refresh ]);
 
   return (
     <div style={{ margin: "0px", padding: "0px", height: "92vh", width: "100%" }}>
